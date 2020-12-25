04890257503ff730a77fd1d26d70f776bb53985c
Matthew Buensalida <buensalidamatthew@gmail.com>
2020-12-25 15:04:36 +0800

    Finished console level 1, added checker function, handled exceptions

--- src/Controller.java
+++ src/Controller.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.GridPane;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.Scanner;
+import java.util.*;
 
 public class Controller {
 
@@ -41,15 +41,24 @@ public class Controller {
 
     private Scanner sc = new Scanner(System.in);
 
-    private char[][] board = new char[3][3];
+    private char[][] board = {{' ', '|', ' ', '|', ' '},
+                              {'-', '+', '-', '+', '-'},
+                              {' ', '|', ' ', '|', ' '},
+                              {'-', '+', '-', '+', '-'},
+                              {' ', '|', ' ', '|', ' '}};
+
+    private List<Integer> playerPos = new ArrayList<>();
+    private List<Integer> aiPos = new ArrayList<>();
 
     private final Image imgX = new Image("x.png");
     private final Image imgO = new Image("o.png");
 
     private int moveCtr = 0;
     private int aiDifficulty;
+    private String winMessage;
     private boolean aiTurn = true;
     private boolean aiFirstMove = true;
+    private boolean gameIsDone = false;
 
     private void initCbx () {
         cbxLevel.setValue("Level 1");
@@ -61,6 +70,7 @@ public class Controller {
     @FXML
     private void gameSetup () {
 
+        btnStart.setDisable(true);
         aiDifficulty = Integer.parseInt(cbxLevel.getValue().substring(6));
         moveList.appendText("Game Start: AI level " + aiDifficulty + "\n");
 
@@ -73,18 +83,116 @@ public class Controller {
         startGame(aiDifficulty);
     }
 
-    private void startGame (int aiDifficulty) {
+    public void startGame (int aiDifficulty) { // Whole game function
 
-
-
+        // Fix loop
+        while(!gameIsDone) {
+            choosePos(aiTurn);
+            aiTurn = !aiTurn;
+            checkWinner();
+            moveCtr++;
+        }
+        System.out.println("The game is done!");
     }
 
+    private void choosePos(boolean aiTurn) {
 
-    @FXML
-    private void sample () {
+        boolean invalid = true;
 
+        Random rand = new Random();
+        int pos = 0;
 
+        while(invalid) {
+            pos = rand.nextInt(9) + 1;
+            if(!aiTurn) {
+                System.out.print("Player turn \nChoose position (1-9): ");
+                pos = sc.nextInt();
+                System.out.println();
+            }
+
+            if(!playerPos.contains(pos) && !aiPos.contains(pos) && (pos < 10 && pos > 0))
+                invalid = false;
+            else if(!aiTurn)
+                System.out.println("Invalid move!");
+        }
+
+        if(!aiTurn) {
+            playerPos.add(pos);
+        }
+        else {
+            System.out.println("AI turn \nAI chose position: " + pos + "\n");
+            aiPos.add(pos);
+        }
+
+        setPos(moveCtr, pos);
+
+        for(char[] row: board) {
+            for(char c: row)
+                System.out.print(c);
+            System.out.println();
+        }
+        System.out.println();
     }
 
+    private void checkWinner () {
 
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+
+        List positiveDiag = Arrays.asList(7, 5, 3);
+        List negativeDiag = Arrays.asList(1, 5, 9);
+
+        List<List> winConditions = new ArrayList<>();
+        winConditions.add(topRow);
+        winConditions.add(midRow);
+        winConditions.add(botRow);
+        winConditions.add(leftCol);
+        winConditions.add(midCol);
+        winConditions.add(rightCol);
+        winConditions.add(positiveDiag);
+        winConditions.add(negativeDiag);
+
+        for(List winCond: winConditions) {
+            if(playerPos.containsAll(winCond)) {
+                gameIsDone = true;
+                System.out.println("You are the winner!");
+            }
+            else if(aiPos.containsAll(winCond)) {
+                gameIsDone = true;
+                System.out.println("You are the loser!");
+            }
+        }
+        if(aiPos.size() + playerPos.size() == 9) {
+            gameIsDone = true;
+            System.out.println("It's a tie!");
+        }
+    }
+
+    private void setPos (int moveCtr, int pos) {
+
+        char symbol;
+
+        if(moveCtr % 2 == 0)
+            symbol = 'X';
+        else
+            symbol = 'O';
+
+        switch (pos) {
+            case 1 -> board[0][0] = symbol;
+            case 2 -> board[0][2] = symbol;
+            case 3 -> board[0][4] = symbol;
+            case 4 -> board[2][0] = symbol;
+            case 5 -> board[2][2] = symbol;
+            case 6 -> board[2][4] = symbol;
+            case 7 -> board[4][0] = symbol;
+            case 8 -> board[4][2] = symbol;
+            case 9 -> board[4][4] = symbol;
+
+        }
+    }
 }
